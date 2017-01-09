af65d3b4fba43d1d566f92ddf2b042286aa9cb94
Evan Hallmark <ehallmark@gttgrp.com>
2017-01-09 10:48:45 -0800

    chagnes

--- src/main/java/IngestGoogleXML.java
+++ src/main/java/IngestGoogleXML.java
@@ -58,7 +58,7 @@ public class IngestGoogleXML {
                     // try non Google
                     try {
                         String dateStr = String.format("%06d", lastIngestedDate);
-                        URL website = new URL(base_url + "/20" + dateStr.substring(0, 2) + "/ipg" + String.format("%06d", lastIngestedDate) + ".zip");
+                        URL website = new URL(secondary_url + "/20" + dateStr.substring(0, 2) + "/ipg" + String.format("%06d", lastIngestedDate) + ".zip");
                         System.out.println("Trying: " + website.toString());
                         ReadableByteChannel rbc = Channels.newChannel(website.openStream());
                         FileOutputStream fos = new FileOutputStream(ZIP_FILE_NAME);
